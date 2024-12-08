e40aeffb746d946fedcb2bf2117b1da32f68e796
LADYFER <ladyfer2022@gmail.com>
2024-12-08 00:01:05 -0500

    wallets reduction part one
    wallets reduction part one

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -21,18 +21,18 @@ const Home: NextPage = () => {
           height: "300px",
           width: "90vw",
           margin: "auto",
-          borderRadius: "24px"         
+          borderRadius: "24px",
+          boxShadow: "1px 13px 19px rgba(133, 130, 130, 0.836)"             
         }}
       >
-        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-30 rounded-3xl">
-          
+        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-30 rounded-3xl">          
           <h1 className="text-white text-4xl font-bold text-center">UN MUNDO SIEMPRE VERDE</h1>
         </div>
       </div>
 
       {/* Contenido principal */}
-      <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="flex flex-col sm:flex-row justify-center items-center gap-8 px-5 w-full">
+      <div className="flex items-center flex-col flex-grow pt-12 pb-8">
+        <div className="flex flex-col sm:flex-row justify-center items-center gap-12 px-5 w-full">
           {/* Billetera Conectada */}
           <div className="flex flex-col items-center">
             <div className="flex justify-center items-center space-x-2 flex-col sm:flex-row">
@@ -40,23 +40,13 @@ const Home: NextPage = () => {
               <Address address={connectedAddress} />
             </div>
 
-            <section className="bg-red-200 w-[90wv] h-64 flex justify-between gap-14 mt-12">
-              <div className="bg-sky-50 w-48 h-48 pt-8 justify-items-center">
+            <section className="bg-red-200 w-[90wv] h-68 pt-8 pb-8 flex justify-between gap-12 mt-12">
+              <div className="bg-sky-100 w-60 h-56 pt-8 pb-8 flex flex-wrap justify-center shadow-xl">
                 {" "}
                 <FaucetButton />
                 <RainbowKitCustomConnectButton />
-              </div>
-              <div className="bg-sky-50 w-48 h-48 pt-8 justify-items-center">
-                {" "}
-                <FaucetButton />
-                <RainbowKitCustomConnectButton />
-              </div>
-              <div className="bg-sky-50 w-48 h-48 pt-8 justify-items-center">
-                {" "}
-                <FaucetButton />
-                <RainbowKitCustomConnectButton />
-              </div>
-              <div className="bg-sky-50 w-48 h-48 pt-8 justify-items-center">
+              </div>             
+              <div className="bg-sky-100 w-60 h-56 pt-8 pb-8 flex flex-wrap justify-center  shadow-xl">
                 {" "}
                 <FaucetButton />
                 <RainbowKitCustomConnectButton />
@@ -68,17 +58,16 @@ const Home: NextPage = () => {
         {/* Sección adicional */}
         <div className="bg-base-300 flex flex-col mt-10 sm:flex-row justify-center items-center gap-8 px-5 w-full">
           <div className="flex justify-center items-center gap-12 mt-10 mb-10 flex-col sm:flex-row">
-            <div className="relative w-80 sm:w-96 h-96 bg-cover bg-center rounded-xl shadow-lg">
+            <div className="relative w-1/2 sm:w-96 h-96 bg-cover bg-center rounded-xl shadow-lg">
               <img
                 src="/images/img5Sowama.png"
                 alt="Sostenibilidad"
-                className="rounded-xl shadow-lg w-full h-96 object-cover"
+                className="rounded-xl shadow-lg w-96 h-96 object-cover"
               />
             </div>           
 
-            <div className="">
-              <p>Somos una empresa BIC dedicada a dar valor a sus residuos orgánicos, 
-              tránsformándolos y recompensándole por ellos.
+            <div className="w-1/2 pl-5">
+              <p className=" text-2xl ">Somos una empresa BIC dedicada a dar valor a sus residuos orgánicos, tránsformándolos y recompensándole por ellos.
               Altamente comprometidos con el medio ambiente.
               </p>
             </div>
